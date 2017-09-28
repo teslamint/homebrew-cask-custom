bdc324c0a5bddcd4c34a644ef00de651efcc1c8d
Jaehoon You <teslamint@gmail.com>
2017-09-28 13:52:54 +0900

    display-maestro: 2.1 (new formula)
    Signed-off-by: Jaehoon You <teslamint@gmail.com>

---
+++ Casks/display-maestro.rb
@@ -0,0 +1,10 @@
+cask 'display-maestro' do
+  version '2.1'
+  sha256 '93610240cd90a52094c58afaeb5c303c099f185c272b4bc64702c4076c2625d1'
+
+  url 'http://mirror.koingosw.com/products/displaymaestro/download/displaymaestro.dmg'
+  name 'Display Maestro'
+  homepage 'http://www.koingosw.com/products/displaymaestro/'
+
+  app 'Display Maestro.app'
+end
